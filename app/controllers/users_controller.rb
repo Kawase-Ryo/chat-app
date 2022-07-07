7fc870c0d9d4e2ff98f6741f876b12f74235d2ee
Kawase-Ryo <2801r.kawase@gmail.com>
2022-07-07 20:44:45 +0900

    ユーザー情報表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,8 @@ class UsersController < ApplicationController
     def edit
     end
     def update
-        if current_user.update(user_params)
-            redirect_to root_path
-            else
-            render :edit
-            end
-        end
-    end
+        current_user.update(user_params)
+      end
     
       private
     
